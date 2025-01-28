e3abb8109dfec9f69aacc7b97ea8f2286c70be62
Claudio Cardoso <claudio.c.lima@hotmail.com>
2025-01-28 17:10:48 -0300

    3. Criando o Projeto

---
+++ README.md
@@ -0,0 +1,13 @@
+## start
+
+-   npx create-next-app@14
+
+## db
+
+-   npm install @prisma/client -S
+-   npm install prisma -D
+-   npx prisma init
+
+## shadcn components
+
+-   npx shadcn@latest init
---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+    return (
+        <div className="">
+            <main className="">
+                <h1 className="font-title">Welcome to ResumeCraft</h1>
+            </main>
+        </div>
+    )
+}
