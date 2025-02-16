b21ea469ff43ee6fb1e57ff306f7ec0a6881581d
Claudio Cardoso <claudio.c.lima@hotmail.com>
2025-02-16 13:26:36 -0300

    16. Seção: Layout

--- src/components/pages/dashboard/resume/resume-structure/index.tsx
+++ src/components/pages/dashboard/resume/resume-structure/index.tsx
@@ -1,9 +1,13 @@
+import { Separator } from "@/components/ui/separator"
 import { TemplatesListSection } from "./sections/templates-list"
+import { LayoutSection } from "./sections/layout"
 
 export const ResumeStructure = () => {
     return (
         <aside className="w-full h-full p-6 overflow-y-auto">
             <TemplatesListSection/>
+            <Separator className="my-5"/>
+            <LayoutSection/>
         </aside>
     )
 }
